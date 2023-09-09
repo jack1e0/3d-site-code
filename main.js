748c98455e1432dc230f46e1ad2d4d923b26a352
Jackie <jackieguo126@gmail.com>
2023-09-09 15:43:11 +0800

    adding text
    commented code: good to know??

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
 
 // 3 things needed for scene: SCENE, CAMERA, RENDERER
-// Note: When first opening folder, run:
-//  npm install
-//  npx vite
+// Note: When first opening folder, run: 'npx vite'
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xdbd5d0);
 
 const camera = new THREE.OrthographicCamera(-1 * window.innerWidth / 16, window.innerWidth / 16, window.innerHeight / 16, -1 * window.innerHeight / 16);
-camera.position.set(30, 15, 42);
+camera.position.set(30, 30, 42);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg')
@@ -46,6 +45,18 @@ scene.add(directionalLightRight);
 
 // ORBIT CONTROL 
 const orbitControl = new OrbitControls(camera, renderer.domElement);
+orbitControl.target.set(1, 20, 0);
+
+orbitControl.rotateSpeed = 0.5;
+orbitControl.maxPolarAngle = Math.PI / 2 - 0.1;
+orbitControl.minPolarAngle = 1;
+orbitControl.minAzimuthAngle = 0.3;
+orbitControl.maxAzimuthAngle = 0.94;
+
+orbitControl.enablePan = false;
+orbitControl.enableZoom = false;
+orbitControl.enableDamping = true;
+orbitControl.dampingFactor = 0.5;
 
 // LOADING
 const loader = new GLTFLoader();
@@ -56,12 +67,78 @@ loader.load('3DCity.glb', function (gltf) {
 
 scene.add(ambientLight);
 
+
+//2D stuff
+
+// Setting up renderer
+// const labelRenderer = new CSS2DRenderer();
+// labelRenderer.setSize(window.innerWidth, window.innerHeight);
+// labelRenderer.domElement.style.position = 'fixed';
+// labelRenderer.domElement.style.top = '0px';
+// labelRenderer.domElement.style.pointerEvents = 'none';
+
+// const para = document.createElement('p');
+// para.textContent = "Welcome!";
+
+// const div = document.createElement('div');
+// div.appendChild(para);
+// const obj = new CSS2DObject(div);
+// scene.add(obj);
+
+// document.body.appendChild(labelRenderer.domElement);
+const d = document.getElementById('text');
+d.style.zIndex = 100;
+d.style.position = 'absolute';
+
+
+function handleMouseScroll(event) {
+    const delta = event.deltaY;
+    if (delta > 0) {
+        if (camera.position.y <= 45) {
+            camera.position.y += delta * 0.01;
+            orbitControl.target.y += delta * 0.01;
+        }
+    } else {
+        if (camera.position.y >= 5) {
+            camera.position.y += delta * 0.01;
+            orbitControl.target.y += delta * 0.01;
+        }
+    }
+}
+
+function updateTextPos() {
+    const vector = new THREE.Vector3();
+    camera.getWorldPosition(vector);
+    vector.project(camera);
+
+    // Map the 3D position to 2D screen coordinates
+    const x = (vector.x + 1) * window.innerWidth / 2;
+    const y = (-vector.y + 1) * window.innerHeight / 2;
+
+    // Update the position of the text element
+    //div.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+function resize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //labelRenderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 function animate() {
     requestAnimationFrame(animate);
+
     renderer.render(scene, camera);
+    //labelRenderer.render(scene, camera);
+
     orbitControl.update();
-    //console.log(camera.rotation);
+    console.log(div.style.transform);
+    //updateTextPos();
 }
+
+window.addEventListener('wheel', handleMouseScroll);
+window.addEventListener('resize', resize);
 
 animate();
 
