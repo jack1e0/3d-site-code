46329e617093e5dc723f1ca7bed3696dc2106d55
Jackie <jackieguo126@gmail.com>
2023-09-09 21:37:28 +0800

    hmm

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css';
+// import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
@@ -89,7 +89,7 @@ function handleMouseScroll(event) {
         }
     }
 
-
+    // Fading header
     if (welc.classList.contains('reveal')) {
         welc.classList.remove('reveal');
 
